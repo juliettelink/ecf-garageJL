680394fd86e49ef63e778d51886f199a06669be1
juliettelinkenheld <linkenheld@hotmail.com>
2023-11-06 14:40:52 +0100

    readme en cours

--- lib/config.php
+++ lib/config.php
@@ -3,11 +3,11 @@
 define("_DOMAIN_", "localhost");
 define("_DB_SERVER_", "localhost");
 define("_DB_NAME_", "garagevp");
-define("_DB_USER_", "root");
-define("_DB_PASSWORD_", "");
+define("_DB_USER_", "sophie");
+define("_DB_PASSWORD_", "aikaanouki");
+
 define("_DEFAULT_IMAGE_FOLDER_", "./uploads/occasion/");
 define("_CARS_IMAGES_FOLDER_", "./uploads/occasion/");
-
 define("_SERVICES_IMAGES_FOLDER_", "./assets/images/");
 define("_HOME_CARS_LIMIT_", 3);
 define("_ADMIN_ITEM_PER_PAGE_", 5);
