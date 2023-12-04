aae689bf5fb712b0a60d5f6692228388cbdcef14
juliettelinkenheld <linkenheld@hotmail.com>
2023-12-04 11:22:25 +0100

    rangement des fichier js

--- templates/footer.php
+++ templates/footer.php
@@ -92,9 +92,9 @@ Si vous avez des questions ou des préoccupations concernant notre politique de 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
 
 <!-- script JS -->
-<script src="filterScript.js"></script>
-<script src="opinionScript.js"></script>
-<script src="private_politicy.js"></script>
+<script src="./assets/js/filterScript.js"></script>
+<script src="./assets/js/opinionScript.js"></script>
+<script src="./assets/js/private_politicy.js"></script>
 
 </body>
 </html>
